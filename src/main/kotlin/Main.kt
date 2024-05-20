42c5dd835d806edcb40a7a3b239dd12c16fb9a87
MakZhon <abovezer0lol@gmail.com>
2024-05-20 09:21:43 +0200

    adding simple test

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -10,10 +10,10 @@ private fun <T> CoroutineScope.executeAllDependencies(
     dependenciesTasks: List<Deferred<T>>,
     action: suspend Deferred<T>.() -> Unit
 ) = dependenciesTasks.forEach { dependency ->
-        launch {
-            dependency.action()
-        }
+    launch {
+        dependency.action()
     }
+}
 
 fun <T> deferredParallelExecution(
     mainTask: Deferred<T>,
@@ -28,36 +28,73 @@ fun <T> deferredParallelExecution(
     return mainTask
 }
 
+fun testDefault() = runBlocking {
+    val task1 = async(start = CoroutineStart.LAZY) {
+        delay(7.seconds)
+        println("Finish execute Task 1")
+        7
+    }
+
+    val task2 = async(start = CoroutineStart.LAZY) {
+        delay(4.seconds)
+        println("Finish execute Task 2")
+        4
+    }
+
+    val dependencies = listOf(task1, task2)
+    val mainTask = async(start = CoroutineStart.LAZY) {
+        dependencies.forEach { dependency ->
+            println("Task value: ${dependency.await()}")
+        }.let { println("All dependencies were executed") }
+    }
+    mainTask.await()
+}
+
+fun testParallel() = runBlocking {
+    val task1 = async(start = CoroutineStart.LAZY) {
+        delay(7.seconds)
+        println("Finish execute Task 1")
+        7
+    }
+    val task2 = async(start = CoroutineStart.LAZY) {
+        delay(4.seconds)
+        println("Finish execute Task 2")
+        4
+    }
+
+    val dependencies = listOf(task1, task2)
+    val mainTask = async(start = CoroutineStart.LAZY) {
+        dependencies.forEach { dependency ->
+            println("Task value: ${dependency.await()}")
+        }.let { println("All dependencies were executed") }
+    }
+
+    deferredParallelExecution(
+        mainTask,
+        dependencies
+    ).await()
+}
+
 fun main() {
     runBlocking {
-        val firstTask = async {
-            delay(7.seconds)
-            println("Finish execute Task 1")
+        measureTime {
+            testDefault()
+        }.let { duration ->
+            if (duration.inWholeSeconds.toInt() == 11) {
+                println("Execute time ${duration.inWholeSeconds}")
+            } else {
+                println("Test 1 not passed")
+            }
         }
-
-        val secondTask = async {
-            delay(4.seconds)
-            println("Finish execute Task 2")
-        }
-
-        val thirdTask = async {
-            delay(6.seconds)
-            println("Finish execute Task 3")
-        }
-
-        val dependencies = listOf(firstTask, secondTask, thirdTask)
 
         measureTime {
-            val mainTask = async {
-                dependencies.forEach{ dependency ->
-                    dependency.await()
-                }.let { println("All dependencies were executed") }
+            testParallel()
+        }.let { duration ->
+            if (duration.inWholeSeconds.toInt() == 7) {
+                println("Execute time ${duration.inWholeSeconds}")
+            } else {
+                println("Test 2 not passed")
             }
-
-            deferredParallelExecution(
-                mainTask,
-                dependencies
-            ).await()
-        }.let { duration -> println("Execute time ${duration.inWholeSeconds}") }
+        }
     }
 }
