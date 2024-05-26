e38895b78ef480049654d619ba7137ad2638c325
MakZhon <abovezer0lol@gmail.com>
2024-05-26 20:55:55 +0200

    version 2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,26 +6,45 @@ import kotlin.coroutines.EmptyCoroutineContext
 import kotlin.time.Duration.Companion.seconds
 import kotlin.time.measureTime
 
+class DeferredParallel<T>(
+    private val mainTask: Deferred<T>,
+    private val dependenciesTasks: List<Deferred<*>>,
+    coroutineContext: CoroutineContext = EmptyCoroutineContext
+) {
+    private var isActive = mainTask::isActive
+    private val parallelScope = CoroutineScope(coroutineContext)
+
+    fun deferredParallelExecution(): Unit {
+        parallelScope.executeAllDependencies(
+            dependenciesTasks, Deferred<*>::start
+        )
+        mainTask.start()
+    }
+
+    fun deferredParallelCancelation(): Unit {
+        parallelScope.executeAllDependencies(
+            dependenciesTasks, Deferred<*>::cancel
+        )
+        mainTask.cancel()
+    }
+
+    suspend fun deferredParallelAwait(): T {
+        if (!isActive.invoke())
+            parallelScope.executeAllDependencies(
+                dependenciesTasks,
+                Deferred<*>::await
+            )
+
+        return mainTask.await()
+    }
+}
+
 private fun <T> CoroutineScope.executeAllDependencies(
-    dependenciesTasks: List<Deferred<T>>,
-    action: suspend Deferred<T>.() -> Unit
+    dependenciesTasks: List<Deferred<T>>, action: suspend Deferred<T>.() -> Unit
 ) = dependenciesTasks.forEach { dependency ->
     launch {
         dependency.action()
     }
-}
-
-fun <T> deferredParallelExecution(
-    mainTask: Deferred<T>,
-    dependenciesTasks: List<Deferred<T>>,
-    coroutineContext: CoroutineContext = EmptyCoroutineContext
-): Deferred<T> {
-    CoroutineScope(coroutineContext).executeAllDependencies(
-        dependenciesTasks,
-        Deferred<T>::start
-    )
-    mainTask.start()
-    return mainTask
 }
 
 fun testDefault() = runBlocking {
@@ -68,11 +87,14 @@ fun testParallel() = runBlocking {
             println("Task value: ${dependency.await()}")
         }.let { println("All dependencies were executed") }
     }
-
-    deferredParallelExecution(
+    var task = DeferredParallel(
         mainTask,
         dependencies
-    ).await()
+    )
+    task.deferredParallelExecution()
+//    task.deferredParallelCancelation()
+//    println(task1.isCancelled)
+//    println(task2.isCancelled)
 }
 
 fun main() {
@@ -94,6 +116,7 @@ fun main() {
                 println("Execute time ${duration.inWholeSeconds}")
             } else {
                 println("Test 2 not passed")
+                println("Execute time ${duration.inWholeSeconds}")
             }
         }
     }
